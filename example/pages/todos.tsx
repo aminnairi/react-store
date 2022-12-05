bc145d22c7951a12bc178dffa1f17be65e77a23a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-12-05 21:23:30 +0100

    updated example

--- example/pages/todos.tsx
+++ example/pages/todos.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, memo } from "react"
 import { TodosForm } from "../components/todos-form"
 import { TodosList } from "../components/todos-list"
-import { Profile } from "../components/profile"
 
 export const TodosPage = memo(() => (
     <Fragment>
