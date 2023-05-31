688f1e146a50bfd65ee9e150449a8766165d7650
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-05-31 14:46:16 +0200

    new example

--- example/store.tsx
+++
@@ -1,8 +0,0 @@
-import { createStore } from "@aminnairi/react-store";
-import { initialState } from "./states/state";
-import { reducer } from "./reducers/reducer";
-
-export const { StoreProvider, useSelector, useDispatch } = createStore({
-    initialState,
-    reducer
-})
